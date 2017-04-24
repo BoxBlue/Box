3a8230a17e3a30affa04cfdf3772023c198d9238
Shreyas <shreyashirday@gmail.com>
2017-04-24 22:39:49 +0000

    added restart

--- server.py
+++ server.py
@@ -6,34 +6,38 @@ import protocol
 backlog = 1
 size = 1024
 
-server_socket = BluetoothSocket(RFCOMM)
-server_socket.bind(("",PORT_ANY))
-server_socket.listen(backlog)
-print("listening")
-dataDict = {}
-uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
 
-advertise_service(server_socket, "BoxBlue",
+def runServer():
+    server_socket = BluetoothSocket(RFCOMM)
+    server_socket.bind(("",PORT_ANY))
+    server_socket.listen(backlog)
+    print("listening")
+    dataDict = {}
+    uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+    advertise_service(server_socket, "BoxBlue",
                             service_id = uuid,
                             service_classes = [ uuid, SERIAL_PORT_CLASS ],
                             profiles = [ SERIAL_PORT_PROFILE ]
-)
+                     )
 
-try:
-    print("waiting to accept")
-    client, clientInfo = server_socket.accept()
-    print("accepting from %s",clientInfo)
-    while 1:
-        print("receiving")
-        data = client.recv(size)
-        if data is None:
-            print("no data")
-        elif data is not None:
+    try:
+        print("waiting to accept")
+        client, clientInfo = server_socket.accept()
+        print("accepting from %s",clientInfo)
+        while 1:
+            print("receiving")
+            data = client.recv(size)
+            if data is None:
+                print("no data")
+            elif data is not None:
             #protocol.aggregate(data, dataDict)
-            retVal = protocol.aggregate(data, dataDict)
-            if retVal is not None:
-                print("responding with " + retVal)
-                client.send(retVal) # Echo back to client.        
-except Exception as err:
-    print("Closing socket:",err)
-    server_socket.close()
+                retVal = protocol.aggregate(data, dataDict)
+                if retVal is not None:
+                    print("responding with " + retVal)
+                    client.send(retVal) # Echo back to client.        
+    except Exception as err:
+        print("Closing socket:",err)
+        server_socket.close()
+        runServer()
+
+runServer()
